1185cb5d8251ac699f850ac3060aad9046cc1a0e
SueleymanOezel <74266172+SueleymanOezel@users.noreply.github.com>
2025-07-12 19:22:39 +0200

    update

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,5 @@
 // frontend/src/main.js
-import "./config"; // lädt WS_URLS, TURN_CONFIG, DEBUG
+import "./config/index.js"; // lädt WS_URLS, TURN_CONFIG, DEBUG
 import "./app.js"; // initialisiert frameLink.core & WebSocketManager
 import "./simple-room.js"; // bindet Room-Logik
 import "./auto-camera-switching.js"; // bindet Face-Detection & Auto-Switch
